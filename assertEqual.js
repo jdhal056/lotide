30ef4181506b6d1fdc34000e8b3a5573ec073a39
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-02 20:34:51 +0000

    Implementing assertEqual assignment

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 5);
+assertEqual("Bootcamp", "Bootcamp");
+
