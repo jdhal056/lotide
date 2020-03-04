f67ec12f0e113ea62884e46545e2d5790614cc77
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-04 20:18:29 +0000

    returns indices of each letter in string

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false; 
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+assertArraysEqual(letterPositions("hello").l, [2,3]);
