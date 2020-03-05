e4158efdd05cf5d4e4ece6d39a8a4c1f80cba9b1
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-05 18:32:41 +0000

    creating my own map function with assertion test

--- map.js
+++ map.js
@@ -10,3 +10,25 @@ const map = function(array, callback) {
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false; 
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+assertArraysEqual(results1, ["g","c","t","m","t"]);
