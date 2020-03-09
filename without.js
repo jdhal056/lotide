030923ff03d185ded2a797174aea28a8c7cdaa96
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:05:03 +0000

    removing assert tests

--- without.js
+++ without.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false; 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const without = function (items, unwantedItems) {
   let newList = [];
   for (let i = 0; i < items.length; i++) {
@@ -28,7 +10,4 @@ const without = function (items, unwantedItems) {
   return newList;
 };
 
-const words = ["hello", "world", "lighthouse"];
-without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
