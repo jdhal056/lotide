d706ae81b2aa0e112bb060493d8914a36b135822
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 15:31:15 +0000

    implementing mocha and chai for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,20 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2,3,4,5]),[4]);
+describe("#middle", function() {
+  it("returns [3] for [1, 2, 3, 4, 5]", function() {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", function() {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns [\"Lighthouse\"] for [\"Hello\", \"Lighthouse\", \"Labs\"]", function() {
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
+  });
+
+  it("returns [\"There\", \"Lighthouse\"] for [\"Hi\", \"There\", \"Lighthouse\", \"Labs\"]", function() {
+    assert.deepEqual(middle(["Hi", "There", "Lighthouse", "Labs"]), ["There", "Lighthouse"]);
+  });
+});
