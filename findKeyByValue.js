cebbdded70a4361c35fdc8642921c45819fab455
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:26:37 +0000

    removing assert tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, value) {
   let keyArr = Object.keys(obj);
   for (let keys of keyArr) {
@@ -15,11 +7,4 @@ const findKeyByValue = function(obj, value) {
   }
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
