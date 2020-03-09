e2403b89357d61ff1dfbfe473b04f722116218f9
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:36:26 +0000

    removing assert tests

--- findKey.js
+++ findKey.js
@@ -9,24 +9,4 @@ const findKey = function(object, callback) {
   return result;
 };
 
-
-// testing
-let objectExample = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}
-let callbackExample = x => x.stars === 2
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(findKey(objectExample, callbackExample),"noma")
+module.exports = findKey;
