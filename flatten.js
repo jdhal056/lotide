671d888a33092da735af637049c2fb8ad00cdd97
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:13:27 +0000

    removing assert tests

--- flatten.js
+++ flatten.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false; 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const flatten = function(array) {
   let flatArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -29,3 +11,5 @@ const flatten = function(array) {
   }
   return flatArray;
 };
+
+module.exports = flatten;
