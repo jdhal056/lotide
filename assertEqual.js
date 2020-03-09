932f259f151fa0dadfd0c3ac12c532da38e3adef
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 13:34:31 +0000

    updated assertEqual with module export

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
-assertEqual("Bootcamp", "Bootcamp");
-
+module.exports = assertEqual;
