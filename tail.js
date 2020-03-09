0053db8aee66331abc4456ba53af6cd854048596
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 13:43:35 +0000

    updating tail to export function

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let tailEnd = [];
   for (let i = 1; i < array.length; i++) {
@@ -14,6 +6,4 @@ const tail = function(array) {
   return tailEnd;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
