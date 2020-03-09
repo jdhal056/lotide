13a56cbe91156d860db2cf8662537ad6b7e07ad2
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:05:34 +0000

    removing assert tests

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -8,27 +6,4 @@ const map = function(array, callback) {
   return results;
 }
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false; 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-
-assertArraysEqual(results1, ["g","c","t","m","t"]);
+module.exports = map;
