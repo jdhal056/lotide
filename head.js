5730a18400a1bbd323e8b6e22e40e4fcd3ac39ec
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 13:34:43 +0000

    updated head with module export

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   let firstItem = "";
@@ -14,5 +8,4 @@ const head = function(array) {
   return firstItem;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
