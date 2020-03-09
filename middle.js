8561f828291a59da7b0137f632a04dbae5866ab0
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 14:18:37 +0000

    editing middle with module export

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false; 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   let newArray = [];
   if (array.length <= 2) {
@@ -30,3 +12,5 @@ const middle = function(array) {
   }
   return newArray;
 };
+
+module.exports = middle;
