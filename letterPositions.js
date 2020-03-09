71d2d0675fa53a8bf2ce75e51b241208a78079b0
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-09 22:05:53 +0000

    removing assert tests

--- letterPositions.js
+++ letterPositions.js
@@ -12,24 +12,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false; 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✔️  Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-
-
-assertArraysEqual(letterPositions("hello").l, [2,3]);
+module.exports = letterPositions;
